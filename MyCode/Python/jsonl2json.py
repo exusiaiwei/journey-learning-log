e4e7b3f4e8d727a6a1fce3fd9851ebd73977a883
wzc <exusiai.wei@outlook.com>
2024-01-13 21:47:33 +0800

    change of structure

--- MyCode/Python/jsonl2json.py
+++
@@ -1,7 +0,0 @@
-import jsonlines
-import json
-
-with jsonlines.open(r"", "r") as rfd:
-    with open(r"", "w", encoding='utf-8') as wfd:
-        for data in rfd:
-            json.dump(data, wfd, indent=4, ensure_ascii=False)
