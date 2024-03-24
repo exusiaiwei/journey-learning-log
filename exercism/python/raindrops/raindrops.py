97ed11d7686a046797de4f47eae23d02111fcee2
wzc <exusiai.wei@outlook.com>
2024-03-24 17:05:44 +0800

    Refactor convert function to simplify logic

--- exercism/python/raindrops/raindrops.py
+++ exercism/python/raindrops/raindrops.py
@@ -1,16 +1,9 @@
 def convert(number):
-    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
-        return "PlingPlangPlong"
-    if number % 3 == 0 and number % 5 == 0 and number % 7 != 0:
-        return "PlingPlang"
-    if number % 3 == 0 and number % 5 != 0 and number % 7 == 0:
-        return "PlingPlong"
-    if number % 3 != 0 and number % 5 == 0 and number % 7 == 0:
-        return "PlangPlong"
-    if number % 3 == 0 and number % 5 != 0 and number % 7 != 0:
-        return "Pling"
-    if number % 3 != 0 and number % 5 == 0 and number % 7 != 0:
-        return "Plang"
-    if number % 3 != 0 and number % 5 != 0 and number % 7 == 0:
-        return "Plong"
-    return str(number)
+    sound = ''
+    if number % 3 == 0: sound += 'Pling'
+    if number % 5 == 0: sound += 'Plang'
+    if number % 7 == 0: sound += 'Plong'
+    if sound == '':
+    # which means can't be divided by 3, 5, or 7
+        sound = str(number)
+    return sound
