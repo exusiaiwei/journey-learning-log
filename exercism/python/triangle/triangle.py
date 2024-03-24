b6a181698940ddbcb4356b79135e7726ebdc4f92
wzc <exusiai.wei@outlook.com>
2024-03-24 16:25:50 +0800

    Refactor triangle validity check and scalene function

--- exercism/python/triangle/triangle.py
+++ exercism/python/triangle/triangle.py
@@ -1,19 +1,9 @@
 def is_valid(sides):
-    side1 = sides[0]
-    side2 = sides[1] 
-    side3 = sides[2]
-
-    if side1 + side2 > side3 and side2 + side3 > side1 and side1 + side3 > side2: 
-        if side1 > 0 and side2 > 0 and side3 > 0:
-            return True
-    return False
+    return all(s != 0 and 2 * s < sum(sides) for s in sides)
 
 def equilateral(sides):
     return is_valid(sides) and len(set(sides)) == 1
-
-
 def isosceles(sides):
     return is_valid(sides) and len(set(sides)) < 3
-
 def scalene(sides):
     return is_valid(sides) and len(set(sides)) == 3
