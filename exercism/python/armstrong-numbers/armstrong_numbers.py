9bbe545b9b7b462da20ff09b19d50f180c8689e2
wzc <exusiai.wei@outlook.com>
2024-03-25 21:54:42 +0800

    ✨ feat(armstrong_numbers.py): Finish exercise
    Complete def is_armstrong_number(number) with string method

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
 def is_armstrong_number(number):
-    pass
+    num_str = str(number)
+    num_length = len(num_str)
+    sum_all = sum(int(c) ** num_length for c in num_str)
+    return sum_all == number
