da8f0bf3ed6b02242b7ab5bb99587e87e9ef1cfc
wzc <exusiai.wei@outlook.com>
2024-03-26 11:58:05 +0800

    🦄 refactor(collatz_conjecture.py): Use Ternary operator
    Refactor collatz_conjecture.py to improve readability and efficiency

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,11 +1,8 @@
 def steps(number):
-    times = 0
+    counter = 0
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
-    while number > 1:
-        if number % 2 == 0:
-            number /= 2
-        else:
-            number = 3 * number + 1
-        times += 1    
-    return times
+    while number != 1:
+        number = number / 2 if number % 2 == 0 else number *3 + 1
+        counter += 1    
+    return counter
