b3a788e7b30c1b0da9922d95721c178a7fcb2f62
wzc <exusiai.wei@outlook.com>
2024-03-26 11:55:46 +0800

    ✨ feat(collatz_conjecture.py): complete exercise
    Finish def steps

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    times = 0
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    while number > 1:
+        if number % 2 == 0:
+            number /= 2
+        else:
+            number = 3 * number + 1
+        times += 1    
+    return times
