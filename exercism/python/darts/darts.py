30ded010605d0aa34659e5be6c2ef6aa5f602422
wzc <exusiai.wei@outlook.com>
2024-03-28 17:24:12 +0800

    🎈 perf(darts.py):

--- exercism/python/darts/darts.py
+++ exercism/python/darts/darts.py
@@ -1,10 +1,10 @@
 def score(x, y):
-    length = x ** 2 + y ** 2
-    if length > 100:
-        return 0
-    elif 25< length <=100:
-        return 1
-    elif 1 < length <=25:
-        return 5
-    else:
+    r = (x ** 2 + y ** 2) ** 0.5
+    if r <=1:
         return 10
+    elif r <= 5:
+        return 5
+    elif r <= 10:
+        return 1
+    else:
+        return 0
