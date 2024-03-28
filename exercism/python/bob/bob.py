0b0dff7ff019ab4048151f19e95a2d3110acc33f
wzc <exusiai.wei@outlook.com>
2024-03-28 16:09:49 +0800

    🦄 refactor(bob.py): refactor the bob.py

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,2 +1,13 @@
 def response(hey_bob):
-    pass
+    hey_bob = hey_bob.strip()
+    if not hey_bob:
+        return 'Fine. Be that way!'
+    elif hey_bob.endswith('?'):
+        if hey_bob.isupper():
+            return 'Calm down, I know what I\'m doing!'
+        else:
+            return 'Sure.'
+    elif hey_bob.isupper():
+        return 'Whoa, chill out!'
+    else:
+        return 'Whatever.'
