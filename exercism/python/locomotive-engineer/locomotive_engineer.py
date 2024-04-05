7d42ef4a1bd5cb65e67364e6609e92c6b14d4e54
wzc <exusiai.wei@outlook.com>
2024-04-05 21:31:35 +0800

    ✨ feat(locomotive_engineer.py): Finish 2 def

--- exercism/python/locomotive-engineer/locomotive_engineer.py
+++ exercism/python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,13 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    return list(args)
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
@@ -17,8 +17,9 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :param missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
-
+    x, y, z, *last = each_wagons_id
+    final_list = [z, *missing_wagons, *last, x, y]
+    return final_list
 
 def add_missing_stops():
     """Add missing stops to route dict.
