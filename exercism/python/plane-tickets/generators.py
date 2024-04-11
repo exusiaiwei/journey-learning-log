e8caaadb800b86b36fc3f87cef02dbb7b3f10305
wzc <exusiai.wei@outlook.com>
2024-04-11 18:58:31 +0800

    ✨ feat(generators.py): Complete exercise

--- exercism/python/plane-tickets/generators.py
+++ exercism/python/plane-tickets/generators.py
@@ -1,6 +1,6 @@
 """Functions to automate Conda airlines ticketing system."""
 
-
+SEAT_LETTERS = ['A', 'B', 'C', 'D']
 def generate_seat_letters(number):
     """Generate a series of letters for airline seats.
 
@@ -13,8 +13,8 @@ def generate_seat_letters(number):
     Example: A, B, C, D
 
     """
-
-    pass
+    for seat in range(number):
+        yield SEAT_LETTERS[seat % 4]
 
 
 def generate_seats(number):
@@ -34,7 +34,23 @@ def generate_seats(number):
 
     """
 
-    pass
+
+    row_number = 1
+    letter_index = 0
+
+
+    for _ in range(number):
+        if row_number == 13:
+            row_number += 1
+
+        seat_number = f"{row_number}{SEAT_LETTERS[letter_index]}"
+        yield seat_number
+
+        letter_index += 1
+        if letter_index == 4:
+            letter_index = 0
+            row_number += 1
+
 
 def assign_seats(passengers):
     """Assign seats to passengers.
@@ -45,8 +61,10 @@ def assign_seats(passengers):
     Example output: {"Adele": "1A", "Björk": "1B"}
 
     """
+    seat_generator = generate_seats(len(passengers))
+    return {passenger: next(seat_generator) for passenger in passengers}
 
-    pass
+
 
 def generate_codes(seat_numbers, flight_id):
     """Generate codes for a ticket.
@@ -56,5 +74,6 @@ def generate_codes(seat_numbers, flight_id):
     :return: generator - generator that yields 12 character long ticket codes.
 
     """
-
-    pass
+    for seat_number in seat_numbers:
+        base_ids = seat_number + flight_id
+        yield (seat_number + flight_id + '0' * (12 - len(base_ids)))
