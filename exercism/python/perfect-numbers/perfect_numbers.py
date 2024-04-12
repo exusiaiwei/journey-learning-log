e997dd07bfca44331d51e011f590d9ae600962da
wzc <exusiai.wei@outlook.com>
2024-04-12 13:12:09 +0800

    ✨ feat(perfect_numbers.py): Complete exercise
    Implement classify function in perfect_numbers.py

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,14 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+    select_number = 0
+    for item in range(1, number):
+        if number % item == 0:
+            select_number += item
+    if select_number == number:
+        return "perfect"
+    if select_number > number:
+        return 'abundant'
+    return 'deficient'
