29d32c6174d0b1b061f1a841b038c9aacad74f71
wzc <exusiai.wei@outlook.com>
2024-04-14 17:08:36 +0800

    ✨ feat(isbn_verifier.py): Complete exercise
    Fix ISBN verification logic in isbn_verifier.py

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,9 @@
 def is_valid(isbn):
-# TODO Finish the function
+    nums = list(isbn.replace("-", ""))
+    if len(nums)!=10:
+        return False
+    if nums[-1] == "X":
+        nums[-1] = "10"
+    if not all([c.isdigit() for c in nums]):
+        return False
+    return sum(int(x)*y for x,y in zip(nums, range(10, 0, -1)))%11 == 0
