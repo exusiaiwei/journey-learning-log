c71e9e0cd39c95ef9967bf7071126ec1cd8f4206
wzc <exusiai.wei@outlook.com>
2024-04-15 14:27:20 +0800

    🐞 fix(wordy.py): Implement answer function
    Add implementation for the answer function in wordy.py

--- exercism/python/wordy/wordy.py
+++ exercism/python/wordy/wordy.py
@@ -1,2 +1,26 @@
+OPS = {
+    "plus": "__add__","minus": "__sub__",
+    "multiplied by": "__mul__","divided by": "__truediv__"
+}
+
 def answer(question):
-    pass
+    question = question.removeprefix("What is").removesuffix("?").strip()
+    if not question: raise ValueError("syntax error")
+    if question.isdigit(): return int(question)
+
+    found_op = False
+    for name, op in OPS.items():
+        if name in question:
+            question = question.replace(name, op)
+            found_op = True
+    if not found_op: raise ValueError("unknown operation")
+
+# process one or more operations
+    ret = question.split()
+    while len(ret) > 1:
+        try:
+            x, op, y, *tail = ret
+            if op not in OPS.values(): raise ValueError("syntax error")
+            ret = [int(x).__getattribute__(op)(int(y)), *tail]
+        except: raise ValueError("syntax error")
+    return ret[0]
