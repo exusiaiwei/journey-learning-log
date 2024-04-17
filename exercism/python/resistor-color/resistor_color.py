5b4a78caac40a44cb5ede87be50f15130cafdea8
wzc <exusiai.wei@outlook.com>
2024-04-17 16:13:30 +0800

    ✨ feat(resistor_color.py): Complete exercise
    Implement color_code and colors function

--- exercism/python/resistor-color/resistor_color.py
+++ exercism/python/resistor-color/resistor_color.py
@@ -1,6 +1,18 @@
+COLOR_DICT = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
 def color_code(color):
-    pass
+    return COLOR_DICT[color]
 
 
 def colors():
-    pass
+    return list(COLOR_DICT.keys())
