4a3ddca64718a0a5b57a26cc5aa625f3f17e7074
wzc <exusiai.wei@outlook.com>
2024-04-18 16:12:31 +0800

    🐞 fix(resistor_color_trio.py): Fix label function in resistor_color_trio.py
    Fix the label function in resistor_color_trio.py to correctly calculate the resistance value and add the appropriate prefix for mega, kilo, and giga ohms.

--- exercism/python/resistor-color-trio/resistor_color_trio.py
+++ exercism/python/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,18 @@
+COLOR_DICT = {
+    "black": 0,"brown": 1,"red": 2,"orange": 3,"yellow": 4,
+    "green": 5,"blue": 6,"violet": 7,"grey": 8,"white": 9,
+}
 def label(colors):
-    pass
+    ohms = (COLOR_DICT[colors[0]] * 10 + COLOR_DICT[colors[1]]) * (10 **  COLOR_DICT[colors[2]])
+    if ohms >= 1000000000:
+        prefix = "giga"
+        ohms //= 1000000000
+    elif ohms >= 1000000:
+        prefix = "mega"
+        ohms //= 1000000
+    elif ohms >= 1000:
+        prefix = "kilo"
+        ohms //= 1000
+    else:
+        prefix = ""
+    return f"{ohms} {prefix}ohms"
