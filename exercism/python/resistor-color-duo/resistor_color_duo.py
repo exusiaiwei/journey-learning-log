042e13f0808ec3f737120ad024124e8c36372a58
wzc <exusiai.wei@outlook.com>
2024-04-18 15:32:11 +0800

    ✨ feat(resistor_color_duo.py): Implement colors function

--- exercism/python/resistor-color-duo/resistor_color_duo.py
+++ exercism/python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,6 @@
+COLOR_DICT = {
+    "black": 0,"brown": 1,"red": 2,"orange": 3,"yellow": 4,
+    "green": 5,"blue": 6,"violet": 7,"grey": 8,"white": 9,
+}
 def value(colors):
-    pass
+    return COLOR_DICT[colors[0]] * 10 + COLOR_DICT[colors[1]]
