47d3432bc514d8328e7955e7564f963073fc43bc
wzc <exusiai.wei@outlook.com>
2024-04-21 17:27:58 +0800

    🐞 fix(binary_search.py): Fix find function
    Fix the find function in binary_search.py to correctly handle the case when the value is not in the search_list. Also, update the logic to recursively search the appropriate half of the list based on the value.

--- exercism/python/binary-search/binary_search.py
+++ exercism/python/binary-search/binary_search.py
@@ -1,2 +1,10 @@
 def find(search_list, value):
-    pass
+    median = len(search_list) // 2
+    if value not in search_list:
+        raise ValueError("value not in array")
+    elif value == search_list[median]:
+        return median
+    elif value < search_list[median]:
+        return find(search_list[:median], value)
+    else:
+        return median + find(search_list[median:], value)
