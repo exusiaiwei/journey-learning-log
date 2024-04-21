63d8e9a84bdc9b87d939b8d50fa810b8e3da31b3
wzc <exusiai.wei@outlook.com>
2024-04-21 17:13:37 +0800

    ✨ feat(house.py):Feat recite function
    Feat the recite function in house.py to generate the correct lyrics for the given start_verse and end_verse. The function now correctly builds the verses by joining the appropriate phrases in reverse order.

--- exercism/python/house/house.py
+++ exercism/python/house/house.py
@@ -1,2 +1,23 @@
 def recite(start_verse, end_verse):
-    pass
+    phrases = [
+        "the house that Jack built",
+        "the malt that lay in",
+        "the rat that ate",
+        "the cat that killed",
+        "the dog that worried",
+        "the cow with the crumpled horn that tossed",
+        "the maiden all forlorn that milked",
+        "the man all tattered and torn that kissed",
+        "the priest all shaven and shorn that married",
+        "the rooster that crowed in the morn that woke",
+        "the farmer sowing his corn that kept",
+        "the horse and the hound and the horn that belonged to",
+    ]
+    lyrics = []
+    for verse in range(start_verse, end_verse + 1):
+        verse_phrases = phrases[0:verse]
+        verse_phrases.reverse()
+        verse_text = "This is " + " ".join(verse_phrases) + "."
+        lyrics.append(verse_text)
+
+    return lyrics
