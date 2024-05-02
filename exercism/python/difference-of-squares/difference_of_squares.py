19aa6c9e04dacaa0a62ea229374f8a620456da58
wzc <exusiai.wei@outlook.com>
2024-05-02 19:53:30 +0800

    ✨ feat(difference_of_squares.py): Implement three functions

--- exercism/python/difference-of-squares/difference_of_squares.py
+++ exercism/python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,16 @@
 def square_of_sum(number):
-    pass
+    result = 0
+    for item in range(1, number+1):
+        result += item
+    return result**2
 
 
 def sum_of_squares(number):
-    pass
+    result = 0
+    for item in range(1, number+1):
+        result += item**2
+    return result
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
