5619eb9cbb312fb8f1cd7eed2150134a0c020e43
wzc <exusiai.wei@outlook.com>
2024-05-20 15:16:09 +0800

    ✨ feat(etl.py): Transform data form

--- exercism/python/etl/etl.py
+++ exercism/python/etl/etl.py
@@ -1,2 +1,7 @@
 def transform(legacy_data):
-    pass
+    """Transform data of type {score: letters} to {letter: score}"""
+    data = {}
+    for k, v in legacy_data.items():
+        for i in v:
+            data[i.lower()] = k
+    return data
