5429960b4cc902a507b4dcd3a5775325d6c856af
wzc <exusiai.wei@outlook.com>
2024-05-28 19:22:03 +0800

    🦄 refactor(gigasecond.py):Refactor gigasecond.py to use timedelta for calculating gigasecond time

--- exercism/python/gigasecond/gigasecond.py
+++ exercism/python/gigasecond/gigasecond.py
@@ -1,9 +1,3 @@
-import datetime
+from datetime import timedelta
 def add(moment):
-    # Convert the moment to seconds using timestamp() method
-    original_seconds = moment.timestamp()
-    # Add 1 gigasecond (10^9 seconds) by adding it to the original seconds
-    total_seconds = original_seconds + 1e9
-    # Convert the total seconds back to a datetime object using fromtimestamp() method
-    final_time = datetime.datetime.fromtimestamp(total_seconds)
-    return final_time
+    return moment + timedelta(seconds=1e9)
