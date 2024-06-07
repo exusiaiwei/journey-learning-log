1e64346ad67472afa87b5d78c0be0e3d1b0384c9
wzc <exusiai.wei@outlook.com>
2024-06-07 18:32:10 +0800

    ✨ feat(start.cmd): Add the cmd script of autogen studio

--- Doing/python/autogen-studio/start.cmd
+++ Doing/python/autogen-studio/start.cmd
@@ -1,2 +1,5 @@
-conda activate autogenstudio
+@echo off
+
+:: Activate the autogenstudio environment and start the UI
+call C:\Scoop\apps\anaconda3\current\App\condabin\conda.bat activate autogenstudio
 autogenstudio ui
